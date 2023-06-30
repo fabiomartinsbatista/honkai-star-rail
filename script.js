1c30ce873aca1b69751460055ddcd4ac5ce6bc22
Fabio Martins Batista <90359745+fabiomartinsbatista@users.noreply.github.com>
2023-06-29 21:40:24 -0300

    new

---
+++ script.js
@@ -0,0 +1,70 @@
+
+// Função de exemplo para manipulação de eventos
+function handleClick() {
+;
+  }
+  
+  // Adiciona um evento de clique ao botão
+  var btn = document.querySelector('.btn');
+  btn.addEventListener('click', handleClick);
+
+
+  function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
+
+function clickMenu() {
+                if (itens.style.display == 'block') {
+                    itens.style.display = 'none'
+                } else {
+                    itens.style.display = 'block'
+                }
+}
+
+
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
